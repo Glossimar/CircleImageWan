e6fbc849ea5df2c95f5fa51eb0b4ab75338e84c7
SunWanQi <wanqing2117@163.com>
2017-03-16 13:24:31 +0800

    gradle更新

--- app/src/main/java/com/bignerdranch/android/circleimagewan/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/circleimagewan/MainActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CircleImageViewWan circleImageViewWan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        circleImageViewWan=(CircleImageViewWan)findViewById(R.id.circle);
     }
 }
